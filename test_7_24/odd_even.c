bbcb5dc0f77110a9e4158d15253fe433f8274a1e
RightRabit <487058764@qq.com>
2021-07-29 20:14:37 +0800

    测试代码

---
+++ test_7_24/odd_even.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*int main() {
+	int i = 1;
+	while (i <= 100) {
+		if (i % 2 == 1)
+			printf("%d ", i);
+			i++;
+	}
+	return 0;
+}*/
+int main() {
+	int i = 1;
+	while (i <= 100) {
+		printf("%d ", i);
+		i += 2;
+		
+	}
+	return 0;
+}
