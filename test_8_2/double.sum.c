c6bbb9cbe5c632d6807c866a3f4f88d1aaf4eeb8
RightRabit <487058764@qq.com>
2021-08-03 15:33:36 +0800

    网课代码

---
+++ test_8_2/double.sum.c
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<math.h>
+//int main()
+//{
+//	int i = 0;
+//	double sum = 0.0;
+//	int flag = 1;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum += flag*1.0 / i;
+//		flag = -flag;
+//	}
+//	printf("sum=%lf", sum);
+//	return 0;
+//}
