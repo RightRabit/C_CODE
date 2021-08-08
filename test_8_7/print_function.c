4e55a5eaeec964898cbe40db6aeca1a54f520883
RightRabit <487058764@qq.com>
2021-08-08 21:20:37 +0800

    递归上

---
+++ test_8_7/print_function.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void print(int n)
+{
+	if (n > 9)
+	{
+		print(n / 10);
+	}
+	printf("%d ", n % 10);
+}
+int main()
+{
+	unsigned int num= 0;
+	scanf("%d", &num);
+	print(num);
+	return 0;
+}
---
+++ test_8_7/strlen_function.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int my_strlen(char* str)
+//{
+//	if (*str != '\0')
+//	{
+//		return 1 + my_strlen(str + 1);
+//	}
+//	else
+//	{
+//		return 0;
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "bit";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
