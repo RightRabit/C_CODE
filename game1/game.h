82095d4b117c9deab6f272dc0bd4bcc96ac981c6
RightRabit <487058764@qq.com>
2021-08-11 14:23:10 +0800

    三子棋

---
+++ game1/game.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define ROW 3
+#define COL 3
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char board[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+char Iswin(char board[ROW][COL], int row, int col);
